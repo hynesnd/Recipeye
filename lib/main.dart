c207cc3ba0c0fd7a88e4b5c5594322341db20e2d
hynesnd <hynesnd@gmail.com>
2022-01-07 22:03:54 +0000

    replace MyApp home with RandomWords widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
+    return const MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
